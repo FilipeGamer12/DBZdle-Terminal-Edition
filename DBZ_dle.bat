f490f4fd3aa4aa64c6b00ad2f4171d90aa8014c6
FilipeGamer12 <74568043+FilipeGamer12@users.noreply.github.com>
2025-12-16 17:35:45 -0300

    [Update]
    melhorias no código e banco de dados.

--- DBZ_dle.bat
+++ DBZ_dle.bat
@@ -8,11 +8,18 @@ Title DBZ dle - Terminal edition
 :Main
 cls
 echo.
+echo ===============================================
+echo      DRAGON BALL Z - DLE TERMINAL EDITION
+echo ===============================================
+echo.
 echo Carregando...
+echo.
 
 if not exist "%raiz%\jsoncmd.exe" set err="jsoncmd.exe não encontrado!" && goto :erro
 if not exist "%raiz%\data.json" set err="data.json não encontrado!" && goto :erro
 if exist "%raiz%\results.cmd" del "%raiz%\results.cmd"
+
+set /a repeat=0
 
 :: --- Carrega nomes
 set i=0
@@ -29,13 +36,26 @@ if %i% EQU 0 (
     pause
     exit /b 1
 )
-
+:random
 :: Seleciona personagem aleatorio
 set /a CHOICE=%RANDOM% %% i + 1
 set "dle=!PERS[%CHOICE%]!"
 
 :: Calcula indice zero-based para usar no json (personagens[IDX])
-set /a IDX=%CHOICE% - 1
+if "%repeat%"=="1" (
+    if exist "%raiz%\no_repeat.cmd" call "%raiz%\no_repeat.cmd"
+    set /a IDX=!CHOICE! - 1
+    if "!IDXold!"=="!IDX!" (
+        set /a CHOICE=%RANDOM% %% i + 1
+        set "dle=!PERS[!CHOICE!]!"
+        set /a IDX=!CHOICE! - 1
+        goto :random
+    )
+) else (
+    del "%raiz%\no_repeat.cmd"
+    set /a repeat=0
+    set /a IDX=!CHOICE! - 1
+)
 
 :: --- Consulta outras chaves do personagem selecionado e salva em variaveis
 :: RACA
@@ -65,15 +85,17 @@ for /f "tokens=1,* delims=:" %%a in ('jsoncmd.exe personagens[%IDX%].habilidade'
 
 :menu
 cls
+echo ===============================================
+echo                 MENU PRINCIPAL
+echo ===============================================
 echo.
-echo Selecione a opção desejada:
+echo  [1] Jogar
+echo  [2] Tutorial
+echo  [3] Listar personagens
+echo  [4] Datacenter
+echo  [5] Sair
 echo.
-echo [1] Jogar
-echo [2] Tutorial
-echo [3] Listar personagens
-echo [4] Datacenter
-echo [5] Sair
-echo.
+echo ===============================================
 
 choice /c 12345 /n /m "Digite> "
 if errorlevel 5 exit
@@ -89,12 +111,18 @@ pause
 
 :game
 cls
-if exist "%raiz%\results.cmd" call "%raiz%\results.cmd"
-echo.
 ::echo %dle%
-echo Tente advinhar o personagem de hoje:
+echo ===============================================
+echo          DRAGON BALL Z - DLE TERMINAL
+echo ===============================================
+echo.
+if exist "%raiz%\results.cmd" call "%raiz%\results.cmd"
+if exist "%raiz%\results.cmd" echo.
+echo Tente advinhar o personagem de Dragon Ball:
 echo.
 set /p try="Digite um nome> "
+echo.
+echo ===============================================
 cls
 goto :analise
 pause
@@ -105,6 +133,13 @@ echo.
 echo Analisando tentativa...
 
 if exist "%raiz%\results.cmd" del "%raiz%\results.cmd"
+
+if "%try%" == "debug" (
+    echo %dle%
+    pause
+    cls
+    goto :game
+)
 
 :: --- encontra índice do try no array PERS
 set "tryIDX="
@@ -118,8 +153,15 @@ if "!tryIDX!"=="%CHOICE%" (
 )
 
 if "!tryIDX!"=="" (
-    echo Personagem "%try%" não encontrado na lista.
-    echo Verifique a ortografia ou tente novamente.
+    cls
+    echo ===============================================
+    echo                NÃO ENCONTRADO
+    echo ===============================================
+    echo.
+    echo O personagem "%try%" não foi encontrado.
+    echo Verifique a grafia e tente novamente.
+    echo.
+    echo ===============================================
     pause
     goto :game
 )
@@ -148,11 +190,20 @@ if "%racaA%"==" None" (
 )
 
 if "!racaA!"=="!racaB!" (
-    echo echo "Raça : !racaA! [=]" >> "%raiz%\results.cmd"
+    echo echo Raça : !racaA! [=] >> "%raiz%\results.cmd"
 ) else (
-    echo echo "Raça : !racaB! [X]" >> "%raiz%\results.cmd"
+    echo echo Raça : !racaB! [X] >> "%raiz%\results.cmd"
 )
 
+:: --- Sexo
+for /f "tokens=1,* delims=:" %%a in ('jsoncmd.exe personagens[%IDX%].Sexo') do set "sexoA=%%b"
+for /f "tokens=1,* delims=:" %%a in ('jsoncmd.exe personagens[%tryIDXjson%].Sexo') do set "sexoB=%%b"
+
+if "!sexoA!"=="!sexoB!" (
+    echo echo Sexo : !sexoA! [=] >> "%raiz%\results.cmd"
+) else (
+    echo echo Sexo : !sexoB! [X] >> "%raiz%\results.cmd"
+)
 
 :: --- pega animeid do personagem sorteado (IDX) e do try (tryIDX)
 for /f "tokens=1,* delims=:" %%a in ('jsoncmd.exe personagens[%IDX%].animeid') do set "animeA=%%b"
@@ -164,12 +215,12 @@ for /f "tokens=1,* delims=:" %%a in ('jsoncmd.exe personagens[%IDX%].anime') do 
 for /f "tokens=1,* delims=:" %%a in ('jsoncmd.exe personagens[%tryIDXjson%].anime') do set "animeTryText=%%b"
 
 if "!animeA!"=="!animeB!" (
-    echo echo "Anime: !animeC! [=]" >> "%raiz%\results.cmd"
+    echo echo Anime: !animeC! [=] >> "%raiz%\results.cmd"
 ) else (
     if !animeA! gtr !animeB! (
-        echo echo "Anime: !animeTryText! [-]" >> "%raiz%\results.cmd"
+        echo echo Anime: !animeTryText! [-] >> "%raiz%\results.cmd"
     ) else (
-        echo echo "Anime: !animeTryText! [+]" >> "%raiz%\results.cmd"
+        echo echo Anime: !animeTryText! [+] >> "%raiz%\results.cmd"
     )
 )
 
@@ -179,12 +230,12 @@ for /f "tokens=1,* delims=:" %%a in ('jsoncmd.exe personagens[%IDX%].saga') do s
 for /f "tokens=1,* delims=:" %%a in ('jsoncmd.exe personagens[%tryIDXjson%].saga') do set "sagaTryText=%%b"
 
 if "!sagaA!"=="!sagaB!" (
-    echo echo "Saga : !sagaC! [=]" >> "%raiz%\results.cmd"
+    echo echo Saga : !sagaC! [=] >> "%raiz%\results.cmd"
 ) else (
     if !sagaA! gtr !sagaB! (
-        echo echo "Saga : !sagaTryText! [-]" >> "%raiz%\results.cmd"
+        echo echo Saga : !sagaTryText! [-] >> "%raiz%\results.cmd"
     ) else (
-        echo echo "Saga : !sagaTryText! [+]" >> "%raiz%\results.cmd"
+        echo echo Saga : !sagaTryText! [+] >> "%raiz%\results.cmd"
     )
 )
 
@@ -207,19 +258,28 @@ for /f "tokens=1,* delims=:" %%a in ('jsoncmd.exe personagens[%IDX%].habilidade'
 :: ANIME
 for /f "tokens=1,* delims=:" %%a in ('jsoncmd.exe personagens[%IDX%].anime') do set "ANIME=%%b"
 
+:: SEXO
+for /f "tokens=1,* delims=:" %%a in ('jsoncmd.exe personagens[%IDX%].Sexo') do set "SEXO=%%b"
+
 :: --- Mostra resultado
 cls
+echo ===============================================
+echo            PARABÉNS!, VOCÊ ACERTOU
+echo ===============================================
 echo.
-echo Parabéns!, você acertou.
+echo  Personagem : %dle%
 echo.
-echo Personagem do dia: %dle%
+echo  Anime      : %ANIME%
+echo  Saga       : %SAGA%
+echo  Raça       : %RACA%
+echo  Sexo       : %SEXO%
+echo  Habilidade : %HABILIDADE%
 echo.
-echo Anime     : %ANIME%
-echo Saga      : %SAGA%
-echo Raça      : %RACA%
-echo Habilidade: %HABILIDADE%
+echo ===============================================
 echo.
 echo Pressione qualquer tecla para voltar ao menu.
+echo set IDXold=%CHOICE% - 1 >> "%raiz%\no_repeat.cmd"
+set /a repeat=1
 pause > nul
 goto :Main
 
@@ -228,14 +288,22 @@ goto :Main
 :tutorial
 cls
 echo.
-echo Você deve tentar advinhar o personagem de Dragon Ball.
-echo Cada rodada, você vai receber dicas de acordo com as suas tentativas.
+echo ================================================
+echo                    TUTORIAL
+echo ================================================
+echo.
+echo Você deve tentar advinhar o personagem aleatório
+echo de Dragon Ball.
+echo Cada rodada, você vai receber dicas de acordo
+echo com as suas tentativas.
 echo Alguns simbolos úteis:
 echo.
 echo [-] Abaixo da correta
 echo [+] Acima da correta
 echo [=] Correto
 echo [X] Errado
+echo.
+echo ===============================================
 echo.
 echo Pressione qualquer tecla para voltar ao menu.
 pause > nul
@@ -257,9 +325,16 @@ goto :menu
 :datacenter
 cls
 echo.
+echo ===============================================
+echo                  DATACENTER
+echo ===============================================
+echo.
 echo Qual personagem você quer mais informações?
 echo.
+echo.
 set /p esc="Digite> "
+echo.
+echo ===============================================
 cls
 echo.
 echo Buscando informações...
@@ -275,6 +350,7 @@ for /f "tokens=1,* delims=:" %%a in ('jsoncmd.exe personagens[%escIDX%].raca') d
 for /f "tokens=1,* delims=:" %%a in ('jsoncmd.exe personagens[%escIDX%].saga') do set "sagaE=%%b"
 for /f "tokens=1,* delims=:" %%a in ('jsoncmd.exe personagens[%escIDX%].habilidade') do set "habilidadeE=%%b"
 for /f "tokens=1,* delims=:" %%a in ('jsoncmd.exe personagens[%escIDX%].anime') do set "animeE=%%b"
+for /f "tokens=1,* delims=:" %%a in ('jsoncmd.exe personagens[%escIDX%].Sexo') do set "sexoE=%%b"
 
 
 if "%escIDX%"=="" (
@@ -297,13 +373,19 @@ if "%racaE%"==" None" (
 
 cls
 echo.
-echo Personagem:  %esc%
-echo Raça      : %racaE%
-echo Habilidade: %habilidadeE%
-echo Anime     : %animeE%
-echo Saga      : %sagaE%
+echo ===============================================
+echo            DATACENTER - %esc%
+echo ===============================================
+echo.
+echo  Raça      : %racaE%
+echo  Sexo      : %sexoE%
+echo  Habilidade: %habilidadeE%
+echo  Anime     : %animeE%
+echo  Saga      : %sagaE%
 echo.
 echo Pressione qualquer tecla para voltar ao menu.
+echo.
+echo ===============================================
 pause > nul
 goto :menu
 
